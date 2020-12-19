125757898fa3de79e65a0e3057fd0bc18c4d1f25
Sahil Nare <sahilnare78@gmail.com>
2020-12-20 02:52:10 +0530

    Fixed redux store and dispatch functions

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './reduxStore';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './components/materialui/theme';
+import './index.css';
 
 ReactDOM.render(
-      <App />,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
